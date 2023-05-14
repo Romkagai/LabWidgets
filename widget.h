d36458702f3ed24e3ee1ee2aa466ab323cf85537
RomanPC <ludoviksoso@gmail.com>
2023-05-14 20:32:24 +0700

    Реализация виджета из задания 2.8

--- widget.h
+++ widget.h
@@ -1,50 +1,35 @@
-#ifndef WIDGET_H
-#define WIDGET_H
-
-#include <QtWidgets> // Для корректной работы всех объектов окна (заместо <QTGui>)
-
-class Widget : public QWidget
+#ifndef widget_h
+#define widget_h
+#include <QtGui>
+class Counter : public QLineEdit
 {
-    Q_OBJECT
-    // QTextCodec *codec;
-    // Нужен для русификации строки (а именно постановки верной кодировки)
-    // Для QT6 данный метод устарел (по умолчанию QT использует кодировку UTF-8)
-    // Проверить на другой машине
-    //      codec = QTextCodec::codecForName("UTF-8");
-    //      setWindowTitle(codec->toUnicode("Возведение в квадрат")); и так далее
-    // P.S. Возможно возвращение работы QTextCodec путем
-    // Установки модуля QT5 Compability Module из установщика
-    // А заместо QTextCodec можно будет воспользоваться QByteArray
-
-    QFrame *frame;              // Рамка
-    QLabel *inputLabel;         // Метка ввода
-    QLineEdit *inputEdit;       // Строчный редактор ввода
-    QLabel *outputLabel;        // Метка вывода
-    QLineEdit *outputEdit;      // Строчный редактор вывода
-    QPushButton *nextButton;    // кнопка Следующее
-    QPushButton *exitButton;    // Кнопка Выход
-
-
+Q_OBJECT
 public:
-    Widget(QWidget *parent = nullptr);  //Конструктор
-
+Counter(const QString & contents, QWidget *parent=0) : QLineEdit(contents,parent){}
+signals:
+void tick_signal();
 public slots:
-    void begin();   // Метод начальной настройки интерфейса
-    void calc();    // Метод реализации вычислений
+void add_one()
+{
+QString str=text();
+int r=str.toInt();
+if (r!=0 && r%5 ==0) emit tick_signal();
+r++;
+str.setNum(r);
+setText(str);
+}
 };
+class Widget: public QWidget
+{
+Q_OBJECT
+protected:
+QTextCodec *codec;
+QLabel *label1,*label2;
+Counter *edit1,*edit2;
+QPushButton *calcbutton;
+QPushButton *exitbutton;
+public:
+Widget(QWidget *parent = 0);
+};
+#endif
 
-// Методичка предлашает использовать Validator - однако по ходу программы от него нет никакого смысла
-// Проверка строки на правильность ввода осуществляется методом calc
-
-//class StrValidator:public QValidator // Класс компонента проверки ввода
-//{
-//public:
-//    StrValidator(QObject *parent):QValidator(parent){}
-//    virtual State validate(QString &str,int &pos)const
-//    {
-//        return Acceptable; // Метод всегда принимает вводимую строку
-//    }
-//};
-
-
-#endif // WIDGET_H
