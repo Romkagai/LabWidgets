5e93fdde2bdb5b46c6c68ffd1809a2f3420ce18e
RomanPC <ludoviksoso@gmail.com>
2023-05-17 21:01:16 +0700

    Обновление под QT6
    Дополнительно рассмотреть случаи утечки памяти

--- widget.h
+++ widget.h
@@ -1,35 +1,37 @@
 #ifndef widget_h
 #define widget_h
-#include <QtGui>
-class Counter : public QLineEdit
-{
-Q_OBJECT
+
+#include <QtWidgets>
+
+class Counter: public QLineEdit {
+  Q_OBJECT
 public:
-Counter(const QString & contents, QWidget *parent=0) : QLineEdit(contents,parent){}
+    Counter(const QString & contents, QWidget * parent = 0): QLineEdit(contents, parent) {}
+
 signals:
-void tick_signal();
+    void tick_signal();
+
 public slots:
-void add_one()
-{
-QString str=text();
-int r=str.toInt();
-if (r!=0 && r%5 ==0) emit tick_signal();
-r++;
-str.setNum(r);
-setText(str);
-}
+    void add_one() {
+      QString str = text();
+      int r = str.toInt();
+      if (r != 0 && r % 5 == 0) emit tick_signal();
+      r++;
+      str.setNum(r);
+      setText(str);
+    }
 };
-class Widget: public QWidget
-{
-Q_OBJECT
-protected:
-QTextCodec *codec;
-QLabel *label1,*label2;
-Counter *edit1,*edit2;
-QPushButton *calcbutton;
-QPushButton *exitbutton;
+
+class Widget: public QWidget {
+  Q_OBJECT
+ protected:
+    // QTextCodec * codec;          //Ситуация с кодеком аналогична задаче 2.7
+    QLabel * label1, * label2;
+    Counter * edit1, * edit2;
+    QPushButton * calcbutton;
+    QPushButton * exitbutton;
+
 public:
-Widget(QWidget *parent = 0);
+    Widget(QWidget * parent = 0);
 };
 #endif
-
